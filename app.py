758f3ad8e2cf174db9d27ad32ad8b416484ba7f7
shubhangiisankar <shubhangiisankar28@gmail.com>
2025-07-07 21:47:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+# Import all the necessary libraries
+import pandas as pd
+import numpy as np
+import streamlit as st
+import pickle
+import joblib
+
+# Load the model and column names
+model = joblib.load("pollution_model.pkl")
+model_cols = joblib.load("model_columns.pkl")
+
+# Let's create a User interface
+st.title("Water Pollutants Predictor")
+st.write("Predict the water pollutants based on Year and Station ID")
+
+# User inputs
+year_input = st.number_input("Enter Year", min_value=2000, max_value=2100, value=2022)
+station_id = st.text_input("Enter Station ID", value='1')
+
+# To encode and then predict
+if st.button('Predict'):
+    if not station_id:
+        st.warning('Please enter the station ID')
+    else:
+        # Prepare the input
+        input_df = pd.DataFrame({'year': [year_input], 'id':[station_id]})
+        input_encoded = pd.get_dummies(input_df, columns=['id'])
+
+        # Align with model cols
+        for col in model_cols:
+            if col not in input_encoded.columns:
+                input_encoded[col] = 0
+        input_encoded = input_encoded[model_cols]
+
+        # Predict
+        predicted_pollutants = model.predict(input_encoded)[0]
+        pollutants = ['O2', 'NO3', 'NO2', 'SO4', 'PO4', 'CL']
+
+        st.subheader(f"Predicted pollutant levels for the station '{station_id}' in {year_input}:")
+        for p, val in zip(pollutants, predicted_pollutants):
+            st.write(f'{p}: {val:.2f}')
